4200fcbacfcfa4eeec6f368dcb5848148610d040
Peter Scully <peter@peterscully.name>
2017-02-20 13:08:05 +1100

    Koan 27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -6,6 +6,9 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
         if (this.month != other.month) return this.month - other.month
         return this.dayOfMonth - other.dayOfMonth
     }
+
+    operator fun rangeTo(d: MyDate): DateRange =
+            DateRange(start = this, endInclusive = d)
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange =

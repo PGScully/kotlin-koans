cf7c3fb7574a80cd67fa5a64066022fa6eb4395c
Peter Scully <peter@peterscully.name>
2017-02-20 17:12:23 +1100

    Koan 41

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -19,12 +19,15 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C : MutableCollection<in T>> Collection<T>.partitionTo(first: C, second: C, selector: (T) -> Boolean): Pair<C, C> {
+    val result = this.partition(selector)
+    return Pair(result.first.toCollection(first), result.second.toCollection(second))
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }

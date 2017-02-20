b57d373e064a579d045e4a72db0d7edd65206b98
Peter Scully <peter@peterscully.name>
2017-02-20 13:27:28 +1100

    Koan 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -6,9 +6,6 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
         if (this.month != other.month) return this.month - other.month
         return this.dayOfMonth - other.dayOfMonth
     }
-
-    operator fun rangeTo(d: MyDate): DateRange =
-            DateRange(start = this, endInclusive = d)
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange =
@@ -21,6 +18,21 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object: Iterator<MyDate> {
+            var current = start
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val temp = current
+                current = current.nextDay()
+                return temp
+            }
+        }
+    }
+
     operator fun contains(d: MyDate): Boolean = (start < d) && (d <= endInclusive)
 }
